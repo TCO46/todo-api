990c350157752825d0366b47f5e032564cb50a40
TCO46 <patohru2006@gmail.com>
2025-08-08 11:29:40 +0700

    chore: Delete test print

--- internal/server/todo/create.go
+++ internal/server/todo/create.go
@@ -2,7 +2,6 @@ package todo
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -32,7 +31,6 @@ func (r *TodoRoutes) CreateTodoHandler(c *gin.Context) {
 	}
 
 	account_id := c.MustGet(middleware.AuthorizationTokenKey).(uuid.UUID)
-	fmt.Print(account_id)
 
 	queries := database.New(r.db)
 	id, err := queries.CreateTodo(ctx, database.CreateTodoParams{
